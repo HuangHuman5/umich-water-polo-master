8e0f924378fedd6641330623c85fd9f17136247f
HuangHuman5 <huangalj@umich.edu>
2026-03-24 11:46:38 -0400

    Updating Everything to Be more Accurate and More Recent Photos

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -13,7 +13,7 @@ import styles from './about.module.css';
 export default function About() {
 	return (
 		<div className={styles.aboutContainer}>
-			<ImageIntro src={`${process.env.PUBLIC_URL}/bannerPhotos/team2.jpg`} />
+			<ImageIntro src={`${process.env.PUBLIC_URL}/bannerPhotos/bigtenwin.jpg`} />
 			<AboutBody />
 		</div>
 	)
